c9382658bc5d6b1779e0144127d92af494d1eb66
jaelyung <isiskny0304@gmail.com>
2021-05-10 23:52:01 +0900

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+function Food({ fav }){
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
-  return <div><h1>Hello!!</h1></div>;
+  return (
+    <div>
+    <h1>Hello!!</h1>
+    <Food fav="kimchi" />
+    <Food fav="ramen" />
+    <Food fav="samgiopsal" />
+    <Food fav="jukumi" />
+    </div>
+  );
 }
 
 export default App;
