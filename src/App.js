5b4a9b4bdc3992e2119eb2414d81c4819d61c5a0
jaelyung <isiskny0304@gmail.com>
2021-06-13 23:46:52 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-function Food({ fav }){
-  return <h1>I like {fav}</h1>;
+function Food({ name, picture }){
+  return (
+    <div>
+        <h1>I like {name}</h1>
+        <h1>I like {picture}</h1>
+    </div>
+ );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "something"
+  },
+  {
+    name: "Mandu",
+    image: "something"
+  },
+  {
+    name: "Salad",
+    image: "something"
+  },
+  {
+    name: "Sandwich",
+    image: "something"
+  }
+];
 
 function App() {
   return (
     <div>
-    <h1>Hello!!</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="jukumi" />
+    {foodILike.map(function(dish) {
+      return <Food name = {dish.name} picture = {dish.image}/>;
+    })}
     </div>
   );
 }
