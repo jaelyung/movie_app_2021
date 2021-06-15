063f105fe8aa59b6f16f59a5e6ce82ffe6377a77
jaelyung <isiskny0304@gmail.com>
2021-06-15 23:57:08 +0900

    0615

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-function Food({ name, picture }){
-  return (
-    <div>
-        <h1>I like {name}</h1>
-        <h1>I like {picture}</h1>
-    </div>
- );
-}
+import React from "react";
 
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image: "something"
-  },
-  {
-    id:2,
-    name: "Mandu",
-    image: "something"
-  },
-  {
-    id:3,
-    name: "Salad",
-    image: "something"
-  },
-  {
-    id:4,
-    name: "Sandwich",
-    image: "something"
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  render(){
+    return (
+    <div>
+      <h1>The number is {this.state.count}</h1>
+      <button>Add</button>
+      <button>Minus</button>
+    </div>
+    
+    )
   }
-];
-
-function renderFood(dish){
-  console.log(dish)
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />
-}
-
-function App() {
-  return (
-    <div>
-    {foodILike.map(renderFood)}
-    </div>
-  );
 }
 
 export default App;
