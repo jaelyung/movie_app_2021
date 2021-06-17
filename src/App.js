b84bcb9b4e59fcdf62c5e2cde7fa67d5e29e9606
jaelyung <isiskny0304@gmail.com>
2021-06-17 23:05:00 +0900

    0617 constructor render componentDidMount componentDidUpdate constructor render componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
@@ -9,9 +13,19 @@ class App extends React.Component{
     this.setState(current => ({count: current.count + 1}));
   };
   minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+    this.setState(current => ({count: current.count - 1})) ;
+  }
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just update");
+  }
+  componentWillUnmount(){
+    console.log("Good bye");
   }
   render(){
+    console.log("I'm rendered!");
     return (
     <div>
       <h1>The number is {this.state.count}</h1>
