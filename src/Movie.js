e39fa1b74c8277ab35d49ab2a1641f0f38be3159
jaelyung <isiskny0304@gmail.com>
2021-06-23 23:01:29 +0900

    render movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie(){
-    return <h1></h1>
+function Movie({id, year, title, summary, poster}){
+    return <h4>{title}</h4>;
 }
 
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+};
+
+export default Movie;
